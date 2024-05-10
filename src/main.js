e71feb30d6259d23e9673edea7ab2776dfd1e7dc
Mapeggs <anguy410@ucsc.edu>
2024-05-10 00:02:36 -0700

    finish stuff

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@
 // Phaser: 3.70.0
 //
 // gallery shooter
+
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    width: 800,
+    height: 640,
+    scene: [shooter]
+}
+
+const game = new Phaser.Game(config);
